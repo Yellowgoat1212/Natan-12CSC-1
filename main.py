77f8089bc5947fcf1495d625f6541b1bff990a4a
Natan Tsegaye <78886208+Yellowgoat1212@users.noreply.github.com>
2022-05-24 23:58:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from tkinter import *
+import random
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+  root = Tk()
+  root.title("Basketball General Knowledge Quiz")
+  quizStarter_object = QuizStarter(root)
+  root.mainloop()
